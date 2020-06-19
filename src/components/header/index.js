5a7c727dbcd95fc2c35f7dfcc66a90059ee5a30e
tusharpandey13 <tusharpandey13@gmail.com>
2020-06-19 10:45:43 +0530

    10:45:43

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -26,7 +26,7 @@ const Header = props => {
   let BlogIcon = <Icon path={mdiBook} size={1} />
   let BlogPostIcon = <Icon path={mdiTextBox} size={1.1} />
   let MenuIcon = <Icon path={mdiMenu} size={1.2} />
-  let cvicon = <Icon path={mdiCardAccountDetailsStarOutline} size={1.2} />
+  let cvicon = <Icon path={mdiCardAccountDetailsStarOutline} size={1.1} />
 
   let nav = (
     <div className={[`H-nav-C`, props.pathIndex == 0 && `blur`].join(" ")}>
@@ -51,7 +51,7 @@ const Header = props => {
 
       {props.pathIndex == 0 && (
         <NavItem
-          href={`https://www.linkedin.com/ambry/?x-li-ambry-ep=AQERcJEq9W2_5wAAAXLIrJ4_p6-PDBbont4CffKb8t4G4wtuIndM2BsAL7sRfnsiU5edNRkexI6rcMnAMn7sdDKb4gmLd4H2Jl7OogS15vMZcgMse8sDu0hMeylzS9t3sD5PEIlpXK4bJREwPbqaezreIPhlXK3VHLomhtXI3rSqcyR7kmFmkPbs_I9B6w2zlVwjaDVnREmHwMbbXuTWc9b8CkTtK6IkQeq5I3Pu-5GRXgKclfhG-cuzkyper4bh12ey1uWR1PdsLtyg5yBGAJ0BvRAnBij4jUC-Qafjl8Goorch3e3oKCJXNlkkilOTLEtUZAW2mp6EmTk4VL0OMMR7VilaRpKcpO3KKM1nYR1bNTYoVuVnmLqHHGB0v3MrSo64qm8oBlVe3fgXIBYekQ5sFRGvhe6vvkF8VLcxd9pVthnIDnv5pbMKs_n1N_5p93hr3d_V25neVn3uSty9MN8WkMQlHdDmdlcaMrvmZgJGBUFjcQ5V0ahAU-VfOcpHETFgK0eilo0gG5dM5D3aRA&x-ambry-um-filename=Profile.pdf`}
+          href={`./profile.pdf`}
           pathIndex={props.pathIndex}
           icon={cvicon}
           text={`Resume`}
