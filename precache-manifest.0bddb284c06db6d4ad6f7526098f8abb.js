c207b4edc47793de1cffa1c6cc27c0a2b8b28489
tusharpandey13 <tusharpandey13@gmail.com>
2020-08-30 09:16:11 +0530

    Updates

---
+++ precache-manifest.0bddb284c06db6d4ad6f7526098f8abb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b2544b207f9d4998e7199438b2fe177",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ff1e4396478e63169c6",
+    "url": "/static/css/main.d206d780.chunk.css"
+  },
+  {
+    "revision": "9ebdb6820890b271d15b",
+    "url": "/static/js/2.ab2b1df2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ab2b1df2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ff1e4396478e63169c6",
+    "url": "/static/js/main.6ed09836.chunk.js"
+  },
+  {
+    "revision": "ec33371a6cfcf1685f95",
+    "url": "/static/js/runtime-main.0bde7a80.js"
+  },
+  {
+    "revision": "1fe603fa054ac66ebd1a21e0176da715",
+    "url": "/static/media/1.1fe603fa.png"
+  }
+]);
