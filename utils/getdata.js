46d58fa1777b8f40c4f915a841761d8500c4c618
tusharpandey13 <tusharpandey13@gmail.com>
2021-06-17 22:14:27 +0530

    Deploy gh-pages

--- utils/getdata.js
+++
@@ -1,8 +0,0 @@
-const data = require('../public/data.json');
-
-const experience = data['experience'];
-const projects = data['projects'];
-const aboutme = data['aboutme'];
-const links = data['links'];
-
-export { experience, projects, aboutme, links };
